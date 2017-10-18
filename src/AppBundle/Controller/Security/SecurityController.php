282206984a73f355c2baf6f1d6a1bd13c39ec94e
= <pintos.ilya@ya.ru>
2017-10-19 02:29:08 +0300

    adding security controller and form

---
+++ src/AppBundle/Controller/Security/SecurityController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\Controller\Security;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request, AuthenticationUtils $authUtils): Response
+    {
+        $error = $authUtils->getLastAuthenticationError();
+
+        $lastUserName = $authUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'lastUsername' => $lastUserName,
+            'error'         => $error,
+        ));
+    }
+}
