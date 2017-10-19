94df59173055992df8432ba52eac5bb3f885d2d3
Artem Gruzinskii <m1nck@yandex.ru>
2017-10-19 12:17:02 +0300

    adding registration, resetting, change password controllers

---
+++ src/AppBundle/Controller/ChangingPassword/ChangePasswordController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller\ChangingPassword;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ChangePasswordController extends Controller
+{
+    /**
+     * @Route("/changePassword")
+     */
+    public function changePasswordAction()
+    {
+
+    }
+
+}
---
+++ src/AppBundle/Controller/Registration/RegistrationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller\Registration;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/register")
+     */
+    public function registerAction()
+    {
+
+    }
+
+    /**
+     * @Route("/checkEmail")
+     */
+    public function checkEmailAction()
+    {
+
+    }
+
+    /**
+     * @Route("/confirm")
+     */
+    public function confirmAction()
+    {
+
+    }
+
+    /**
+     * @Route("/confirmed")
+     */
+    public function confirmedAction()
+    {
+
+    }
+
+}
