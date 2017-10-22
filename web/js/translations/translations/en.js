b7aa45b23b4755d55a90173b5a525c91581b371f
tishonokItra <tishonok.ilya@ya.ru>
2017-10-22 20:58:03 +0300

    adding translator bundle to js

---
+++ web/js/translations/translations/en.js
@@ -0,0 +1,12 @@
+(function (t) {
+// en
+t.add("security.login.login_label", "Sign in to access your account", "translations", "en");
+t.add("security.login.username", "Username", "translations", "en");
+t.add("security.login.password", "Password", "translations", "en");
+t.add("security.login.remember_me", "Remember me", "translations", "en");
+t.add("security.login.submit", "Sign in", "translations", "en");
+t.add("security.login.register", "Sign Up", "translations", "en");
+t.add("security.login.register_question", "Don't have account yet?", "translations", "en");
+t.add("security.login.resetting", "Forgot password", "translations", "en");
+t.add("security.registration.registration_label", "Sign in to access your account", "translations", "en");
+})(Translator);
