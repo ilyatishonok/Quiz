2b14176a83c59c29f3675f1a7d5ae87a6ba66cd4
tishonokItra <tishonok.ilya@ya.ru>
2017-11-05 15:26:02 +0300

    adding filtering and sorting to usesr manager

--- src/AppBundle/Controller/Home/HomeController.php
+++ src/AppBundle/Controller/Home/HomeController.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace AppBundle\Controller\Home;
 
+use AppBundle\Repository\CompletedQuizRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
@@ -35,6 +38,15 @@ class HomeController extends Controller
      */
     public function showStartedQuizesAction(Request $request)
     {
+        $quizRepository = $this->getDoctrine()->getManager()->getRepository("AppBundle\Entity\Quiz");
+        $quiz = $quizRepository->findOneBy(array("id"=>10));
+        /** @var CompletedQuizRepository $completedQuizRep */
+        $completedQuizRep = $this->getDoctrine()->getManager()->getRepository("AppBundle\Entity\CompletedQuiz");
+        $compleQuiz = $completedQuizRep->findOneBy(array("user"=>$this->getUser(), "quiz"=>$quiz));
+        /** @var \DateInterval $time */
+        $time = $compleQuiz->getTime();
+        //$pos = $completedQuizRep->loadUserPosition($compleQuiz->getRightQuestions(),$compleQuiz->getTime()->);
+        exit(dump($time));
         return $this->render("base.html.twig");
     }
 }
